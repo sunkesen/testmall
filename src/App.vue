62199bef79dc237620628f82f8a4f4929e831e70
sunkesen <277908391@qq.com>
2022-01-11 13:48:59 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+git remote add origin https://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@github.com/sunkesen/supermall.git
+<!--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-->
+<!--277908391@qq.com-->
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+// git remote set-url ori remote set-url origin ghttps://github.com/sunkesen/supermall.git
+// git clone https://github.com/sunkesen/supermall.git
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
